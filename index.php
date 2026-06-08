a68f239953e6b210fa9b65111babe414754495da
Elio-byte <elio.keputa@icloud.com>
2026-06-08 18:34:55 +0200

    Add index.php for Formula Team Manager 1970

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Formula Team Manager 1970</title>
+</head>
+<body>
+
+<h1>Formula Team Manager 1970</h1>
+
+<p>Benvenuto Team Principal!</p>
+
+<a href="register.php">Registrati</a>
+<br><br>
+<a href="login.php">Login</a>
+
+</body>
+</html>
